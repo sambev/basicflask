0709e38f4e81131f523a17f841aad42a2a64dd5c
Sam Beveridge <sam.bev87@gmail.com>
2014-02-24 23:10:26 -0700

    Made a much better way to scale the app and separate views for url mappings

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, Response, render_template
 from config.jinjacfg import setUpJinjaEnv
 from util.pbkdf2 import pbkdf2_hex
 from util.salts import getRandomSalt
+from controllers.auth import login, signup
 
 
 app = Flask(__name__)
@@ -28,68 +29,8 @@ def buildApp(env='dev'):
         if request.method == 'GET':
             return render_template('index.html')
 
-
-    @app.route("/login")
-    def login():
-        """ Endpoint for authentication
-            *Requires some sort of database
-        """
-        if request.method == 'POST':
-            # Get the user information
-            name = request.form['username']
-            passwd = request.form['userpass']
-
-            # Find a user with that username an compare passwords
-            res = db.users.find({ 'name': name })
-            if res.count() > 0:
-                user = parseMongoResponse(res)[0]
-                if user:
-                    salt = user['salt']
-                    thehash = pbkdf2_hex(passwd.encode('utf-8'), salt.encode('utf-8'))
-                else:
-                    error = 'Invalid Credentials'
-                    return render_template('home.html', { 'error': error })
-
-                if thehash == user['hash']:
-                    # store user id in the session
-                    session['user'] = user['name']
-                    return redirect('/company_list')
-                else:
-                    error = 'Invalid Credentials'
-                    return render_template('home.html', { 'error': error })
-            else:
-                error = 'Invalid Credentials'
-                return render_template('home.html', { 'error': error })
-
-
-    @app.route('/signup')
-    def signup():
-        """ End Point for signups
-            *Requires some sort of database
-        """
-        if request.method == 'GET':
-            return render_template('signup.html')
-
-        if request.method == 'POST':
-            if request.form['userpass'] != request.form['userpass2']:
-                error = 'Passwords do not match'
-                return render_template('signup.html', { 'error': error })
-
-            salt =  getRandomSalt(16)
-            thehash = pbkdf2_hex(request.form['userpass'].encode('utf-8'), salt.encode('utf-8'))
-
-            # Make a new user out of the info
-            new_user = {
-                'name': request.form['username'],
-                'salt': unicode(salt),
-                'hash': unicode(thehash)
-            }
-
-            user_id = db.users.save(new_user)
-            # store user id in the session
-            session['user'] = new_user['name']
-
-            return redirect('/home')
+    app.add_url_rule('/login', 'login', login)
+    app.add_url_rule('/signup', 'signup', signup)
 
     return app
 
