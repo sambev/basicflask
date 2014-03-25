0736454f40f629a16dd5a61a2ba932ad69e518be
Sam Beveridge <sam.bev87@gmail.com>
2014-03-24 22:12:32 -0600

    add basic view

---
+++ api_views/user.py
@@ -0,0 +1,23 @@
+from flask.views import MethodView
+
+class UserAPI(MethodView):
+
+    def get(self, user_id):
+        if user_id is None:
+            # return a list of users
+            pass
+        else:
+            # expose a single user
+            pass
+
+    def post(self):
+        # create a new user
+        pass
+
+    def delete(self, user_id):
+        # delete a single user
+        pass
+
+    def put(self, user_id):
+        # update a single user
+        pass
